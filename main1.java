2800ebf61329ea4fa888fc60e99e0316c75bb79d
Ye-eun-Shin <50901031+Ye-eun-Shin@users.noreply.github.com>
2019-09-19 20:57:51 +0900

    Add files via upload

---
+++ main1.java
@@ -0,0 +1,13 @@
+package com.example.ruby0_000.study_ranking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
